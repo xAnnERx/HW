aa0d4988caa828b861256a18398694a4668cb9b9
xAnnERx <carpediem.2018@mai.ru>
2024-08-08 13:35:36 +0300

    fix hw

--- src/HW/4/Input.tsx
+++ src/HW/4/Input.tsx
@@ -1,14 +1,20 @@
 type InputPropsType = {
-	currentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-	setCurrentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+  currentText: string; // НУЖНО ПРОТИПИЗИРОВАТЬ
+  setCurrentText: (value: string) => void; // НУЖНО ПРОТИПИЗИРОВАТЬ
 };
 
 export const Input = (props: InputPropsType) => {
-	const onChangeHandler = (event: 'НУЖНО ПРОТИПИЗИРОВАТЬ') => {
-		// НУЖНО ДОПИСАТЬ
-	};
+  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    props.setCurrentText(event.currentTarget.value);
+    // НУЖНО ДОПИСАТЬ
+  };
 
-	// return (
-	//   <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
-	// );
+  return (
+    <input
+      id={"hw04-input"}
+      type="text"
+      value={props.currentText}
+      onChange={onChangeHandler}
+    />
+  );
 };
